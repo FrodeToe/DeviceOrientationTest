878f02d7c223bb5c564804abd306a2201b7dfca9
Tørresdal Frode <Frode.Torresdal@norconsult.com>
2017-02-23 09:06:54 +0100

    First version

---
+++ ISY.Extension.DeviceOrientation.js
@@ -0,0 +1,114 @@
+var _viewer;
+var _rotInterval = {lastx:0,lasty:90,lastz:0}; 
+
+function ISYExtensionDeviceOrientation(viewer, options) {
+  Autodesk.Viewing.Extension.call(this, viewer, options);
+
+  _viewer = viewer;
+}
+
+ISYExtensionDeviceOrientation.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
+
+ISYExtensionDeviceOrientation.prototype.load = function () {
+  console.log('ISYExtensionDeviceOrientation loaded');
+
+  if(window.DeviceOrientationEvent){
+      _rotInterval = {lastx:0,lasty:90,lastz:0}; 
+      window.addEventListener("deviceorientation", orientationListener, false); 
+  }else
+  {
+      alert("DeviceOrientationEvent is not supported");
+  }
+
+  return true;
+}
+
+ISYExtensionDeviceOrientation.prototype.unload = function () {
+  console.log('ISYExtensionDeviceOrientation unloaded');
+
+  window.removeEventListener('deviceorientation', orientationListener);
+
+  return true;
+}
+
+ function orientationListener(event){
+    //rotInterval = {lastx:0,lasty:0,lastz:0}; 
+
+    var alpha = parseInt(event.alpha);
+    var beta = parseInt(event.beta);
+    var gamma = parseInt(event.gamma);
+
+    document.getElementById("alpha").innerHTML="Alpha="+alpha;
+    document.getElementById("beta").innerHTML="Beta="+beta;
+
+    // get camera
+    var cam = _viewer.getCamera();
+
+ /*   newx = event.alpha;
+    newy = event.beta;
+    newz = event.gamma;  
+
+    //rotate the camera
+    var localCam = cam.clone();
+    var newPosition = localCam.position;
+    var newTarget = localCam.target;
+    var directionFwd = cam.target.clone().sub(cam.position);
+    var directionRight = directionFwd.clone().cross(cam.up).normalize();
+
+    //rotate around up vector
+    var yawX = new THREE.Quaternion();
+    var changed = ( Math.abs(newx-_rotInterval.lastx) >1 );
+    if(changed) 
+      yawX.setFromAxisAngle(localCam.up, 2*Math.PI*(newx-_rotInterval.lastx)/360);
+
+    //rotate around right vector
+    var yawY= new THREE.Quaternion();
+    changed = ( Math.abs(newy-_rotInterval.lasty) >1 );
+    if(changed) 
+      yawY.setFromAxisAngle(directionRight, 2*Math.PI*(newy-_rotInterval.lasty)/360);
+
+    var yawQ = new THREE.Quaternion();
+    yawQ.multiply(yawX).multiply(yawY);//.multiply(yawZ);
+
+    directionFwd.applyQuaternion(yawQ);
+    localCam.up.applyQuaternion(yawQ);
+    var _navapi = _viewer.navigation;
+    newTarget = newPosition.clone().add(directionFwd);
+    _navapi.setView(newPosition, newTarget);
+    _navapi.orientCameraUp(); 
+
+    _rotInterval = {lastx:newx,lasty:newy,lastz:newz};  
+*/
+    // get position and target
+    var position = cam.position.clone();
+    var target = cam.target.clone();
+
+    // get view vector
+    var vecViewDir = new THREE.Vector3();
+    vecViewDir.subVectors(target,position);
+
+    // get length of view vector
+    var length = vecViewDir.length();
+
+    // rotate alpha
+    var vec = new THREE.Vector3();
+    vec.y = length;
+    var zAxis = new THREE.Vector3(0,0,1);
+    vec.applyAxisAngle(zAxis,THREE.Math.degToRad(alpha));
+
+    // rotate beta
+    var vec2 = new THREE.Vector3(vec.x,vec.y,vec.z);
+    vec2.normalize();
+    vec2.negate();
+    vec2.cross(zAxis);
+    vec.applyAxisAngle(vec2,THREE.Math.degToRad(beta) + Math.PI / 2);
+
+    // add to camera
+    target = target.addVectors(position,vec);
+    _viewer.navigation.setView (position, target);
+    _viewer.navigation.setCameraUpVector (zAxis) ;
+    //_viewer.applyCamera(cam,false);
+}
+
+
+Autodesk.Viewing.theExtensionManager.registerExtension('ISYExtensionDeviceOrientation', ISYExtensionDeviceOrientation);
